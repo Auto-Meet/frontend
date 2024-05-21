621b1755bd95830b4bbf6bebf555dd05c60bedbf
taek <yeontaek94@naver.com>
2024-05-21 13:19:47 +0900

    chore: add routes

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import Room from "./pages/meetingRoom/Room.jsx";
 import Create from "./pages/meetingRoom/Create.jsx";
+import Join from "./pages/meetingRoom/Join.jsx";
+import Record from "./pages/Record.jsx";
 
 function App() {
   return (
@@ -15,6 +17,8 @@ function App() {
             <Route path="/home" element={<Home />} />
             <Route path="/room" element={<Room />} />
             <Route path="/createRoom" element={<Create />} />
+            <Route path="/joinRoom" element={<Join />} />
+            <Route path="/meetingRecord" element={<Record />} />
           </Routes>
         </BrowserRouter>
       </div>
