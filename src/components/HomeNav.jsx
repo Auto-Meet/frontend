0930edaa68628daf0c4f2ea8597b4a8190fc1f64
taek <yeontaek94@naver.com>
2024-05-22 17:47:37 +0900

    feat: 홈화면 로그아웃 및 로그인

--- src/components/HomeNav.jsx
+++ src/components/HomeNav.jsx
@@ -1,5 +1,7 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styled from "styled-components";
+import Swal from "sweetalert2";
 
 const Logo = styled.h1`
   font-size: 40px;
@@ -15,15 +17,18 @@ const Name = styled.p`
 const LogOut = styled.p`
   font-size: 17px;
   color: #112d4e;
+  cursor: pointer;
 `;
 
 const Box = styled.div`
   display: flex;
   flex-direction: row;
-  justify-content: space-evenly;
+  justify-content: space-between;
   align-items: center;
   margin-top: 10vh;
   margin-bottom: 3vh;
+  margin-left: 17vw;
+  margin-right: 17vw;
 `;
 
 const LBox = styled.div`
@@ -42,6 +47,42 @@ const Empty = styled.div`
 `;
 
 const HomeNav = () => {
+  const [logCheck, setLogCheck] = useState(false);
+  const [name, setName] = useState("");
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem("token")) {
+      setLogCheck(true);
+      setName(localStorage.getItem("name"));
+    } else setLogCheck(false);
+  }, []);
+
+  const handleLog = () => {
+    if (logCheck) {
+      Swal.fire({
+        title: "로그아웃",
+        text: "로그아웃을 하시겠습니까?",
+        icon: "question",
+
+        showCancelButton: true,
+        confirmButtonColor: "#1F316F",
+        cancelButtonColor: "#b1b1b1",
+        confirmButtonText: "네",
+        cancelButtonText: "아니요",
+        reverseButtons: true,
+      }).then((result) => {
+        if (result.isConfirmed) {
+          localStorage.removeItem("token");
+          setLogCheck(false);
+        }
+      });
+    }
+
+    if (!logCheck) {
+      navigate("/login");
+    }
+  };
   return (
     <>
       <Box>
@@ -51,8 +92,14 @@ const HomeNav = () => {
         </LBox>
         <Empty />
         <RBox>
-          <Name>이연택님 안녕하세요.</Name>
-          <LogOut>로그아웃</LogOut>
+          <Name>
+            {logCheck
+              ? `${name}님 안녕하세요.`
+              : "지금 로그인을 통해, 서비스를 이용해보세요."}
+          </Name>
+          <LogOut onClick={() => handleLog()}>
+            {logCheck ? "로그아웃" : "로그인"}
+          </LogOut>
         </RBox>
       </Box>
     </>
